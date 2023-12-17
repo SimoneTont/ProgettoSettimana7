f5b4e2f3899f0c5427602ee3d4dcf80eed0a39fd
Simone Tonti <simone.tonti98@gmail.com>
2023-12-17 13:46:47 +0100

    refresh button

--- js/scriptBackOffice.js
+++ js/scriptBackOffice.js
@@ -45,11 +45,6 @@ SubmitButton.addEventListener('click', () => {
     }else
     {
         let obj= new Item (F[0].value, F[1].value, F[2].value, F[3].value, F[4].value)
-        F[0].value =""
-        F[1].value =""
-        F[2].value =""
-        F[3].value =""
-        F[4].value =""
         console.log(obj)
 
         fetch(URL, {
@@ -63,6 +58,16 @@ SubmitButton.addEventListener('click', () => {
     }
 })
 
+//Form Reset
+let FormReset = document.querySelector("#ResetFormButton")
+FormReset.addEventListener('click', () => {
+    F[0].value =""
+    F[1].value =""
+    F[2].value =""
+    F[3].value =""
+    F[4].value =""
+})
+        
 // Chiamata di tipo POST
 /* 
 fetch(URL, {
@@ -74,30 +79,25 @@ fetch(URL, {
     }
 });
 */
+let ShowButton = document.querySelector("#ShowButton")
+let container = document.querySelector('#CardsContainer');
+console.log(container)
+ShowButton.addEventListener('click', () => {
+    container.innerHTML=""
+    //Chiamata GET
+    fetch(URL, {
+        method: "GET", // Method della chiamata 
+        headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": STRIVE_SCHOOL_API_KEY
+        }
+    }).then(response => response.json()).then(json => json.forEach(element => {CreateCard(element)}))
+})
 
-//Chiamata GET
-fetch(URL, {
-    method: "GET", // Method della chiamata 
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-      "Authorization": STRIVE_SCHOOL_API_KEY
-      }
-  }).then(response => response.json()).then(json => json.forEach(element => { CreateCard(element)}))
 
-//Chiamata DELETE
-/*
-fetch(URL +"/"+ItemID, {
-method: "DELETE", // Method della chiamata 
-headers: {
-    "Content-type": "application/json; charset=UTF-8",
-    "Authorization": STRIVE_SCHOOL_API_KEY
-    }
-});*/
-
+//Card creation
 function CreateCard (Obj)
 {
-    let container = document.querySelector('#CardsContainer');//Seleziona container
-
     let CardDiv = document.createElement("div");//Crea CardDiv
     container.appendChild(CardDiv);//Div append a container
     CardDiv.classList.add("card", "m-4");
@@ -107,6 +107,7 @@ function CreateCard (Obj)
     imag.classList.add("img-fluid");
     CardDiv.appendChild(imag);//imag dentro CardDiv
     imag.src=Obj.imageUrl;
+    imag.alt=Obj._id;
 
     let CardBody = document.createElement("div");//Crea CardBody
     CardBody.classList.add("card-body");
@@ -122,24 +123,30 @@ function CreateCard (Obj)
     CardBody.appendChild(brandName);
     brandName.innerText=Obj.brand;
 
+    let ItemDescription = document.createElement("p");
+    ItemDescription.classList.add("card-text");
+    CardBody.appendChild(ItemDescription);
+    ItemDescription.innerText=Obj.description
+
     let cost = document.createElement("p");
     cost.classList.add("card-text");
     CardBody.appendChild(cost);
     cost.innerText=Obj.price +"€";
 
     let deletionButton = document.createElement("button");
-    deletionButton.classList.add("btn","btn-danger", "d-none");
+    deletionButton.classList.add("btn","btn-danger", "d-none", "DeletionButton");
     CardBody.appendChild(deletionButton);
     deletionButton.innerText="Delete from server"
 
     let editingButton = document.createElement("button");
-    editingButton.classList.add("btn","btn-success", "d-none");
+    editingButton.classList.add("btn","btn-success", "d-none", "EditButton");
     CardBody.appendChild(editingButton);
     editingButton.innerText="Edit item"
 
     console.log(Obj)
 }
 
+//Advanced Options Button
 let AOButton = document.querySelector("#AdvancedOptionsButton")
 
 AOButton.addEventListener('click', () => {
@@ -147,4 +154,33 @@ AOButton.addEventListener('click', () => {
     advancedButtons.forEach(element => {
         element.classList.remove("d-none")
     });
+
+    let deletionButtons = document.querySelectorAll(".DeletionButton")
+    deletionButtons.forEach(element => {
+        element.addEventListener ('click', () => {DeleteItem(element)})
+    });
+    
+
+    console.log(deletionButtons)
+
+    let editButtons = document.querySelectorAll(".EditButton")
+    console.log(editButtons)
 });
+
+//Delete function
+function DeleteItem(currentButton)
+{
+    //Chiamata DELETE
+    let CurrentCard=currentButton.parentElement.parentElement
+    let CurrentID=CurrentCard.querySelector("img").alt
+    //console.log(CurrentID)
+    fetch(URL +"/"+CurrentID, {
+    method: "DELETE", // Method della chiamata 
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": STRIVE_SCHOOL_API_KEY
+        }
+    });
+    CurrentCard.remove()
+}
+
